dfa2430ba529ad7aa456e80b12c63a092fb73e15
Niki <Niki23091999@gmail.com>
2020-11-06 10:02:19 +0100

    1

---
+++ Zahlenconverter/Konverter.feature
@@ -0,0 +1,21 @@
+﻿Feature: Konverter
+	As a  nerd
+	I want to koncert strings to integer
+
+@mytag
+Scenario: hex
+	Given the srtig is <affe>
+	When the the string gets converted with base sixteen
+	Then the result should be 45054
+
+@mytag
+Scenario: oct 
+	Given the srtig is <11147>
+	When the the string gets converted with base eight
+	Then the result should be 4711
+
+	@mytag
+Scenario:bin 
+	Given the srtig is <1100>
+	When the the string gets converted with base two
+	Then the result should be 12
---
+++ Zahlenconverter/KonverterSteps.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace Zahlenconverter
+{
+    [Binding]
+    public class KonverterSteps
+    {
+
+        string x;
+        int Integer;
+        [Given(@"the srtig is (.*)")]
+        public void GivenTheSrtigIs(string p0)
+        {
+            x = p0;
+        }
+        
+        [When(@"the the string gets converted with base sixteen")]
+        public void WhenTheTheStringGetsConvertedWithBaseSixteen()
+        {
+            Integer = System.Convert.ToInt32(x, 16);
+        }
+        
+        [When(@"the the string gets converted with base eight")]
+        public void WhenTheTheStringGetsConvertedWithBaseEight()
+        {
+            Integer = System.Convert.ToInt32(x, 8);
+        }
+        
+        [When(@"the the string gets converted with base two")]
+        public void WhenTheTheStringGetsConvertedWithBaseTwo()
+        {
+            Integer = System.Convert.ToInt32(x, 2);
+        }
+        
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, Integer);
+        }
+    }
+}
